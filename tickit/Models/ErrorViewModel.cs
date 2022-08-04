9513b9aba2c246698dd59d6884573692babccca4
obaida <obaida@DESKTOP-QALQS68>
2022-08-04 20:36:08 +0300

    Add project files.

---
+++ tickit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tickit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
